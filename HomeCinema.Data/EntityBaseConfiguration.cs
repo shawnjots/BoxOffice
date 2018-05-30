8a6be04d0754916b3f102758f4b2b1f3b0705ba4
Shawn Majoto <shawnjots@gmail.com>
2018-05-30 17:14:52 +0200

    Created class library for entity configuration.

---
+++ HomeCinema.Data/EntityBaseConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using HomeCinema.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace HomeCinema.Data
+{
+    public class EntityBaseConfiguration<T> : IEntityTypeConfiguration<T> where T : class, IEntityBase
+    {
+        public void Configure(EntityTypeBuilder<T> builder)
+        {
+            
+        }
+    }
+}
