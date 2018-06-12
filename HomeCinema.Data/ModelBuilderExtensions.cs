7802400e401e3dd26623c7026e676dd0a30360a1
Shawn Majoto <shawnjots@gmail.com>
2018-06-12 21:32:09 +0200

    Added appropriate inheritance for configuration classes. Extended ModelBuilder type.

---
+++ HomeCinema.Data/ModelBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCinema.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void RemovePluralizingTableNameConvention(this  ModelBuilder modelBuilder)
+        {
+            foreach (IMutableEntityType entity in modelBuilder.Model.GetEntityTypes())
+            {
+                //entity. = entity.DisplayName(); Find method tablename
+            }
+        }
+    }
+}
